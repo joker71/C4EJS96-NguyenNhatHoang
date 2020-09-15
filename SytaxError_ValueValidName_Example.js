154c234f38191972f482bb89d79090c1c80b1d6c
joker71 <nhathoangn0@gmail.com>
2020-09-15 12:26:09 +0700

    add

--- SytaxError_ValueValidName_Example.js
+++ SytaxError_ValueValidName_Example.js
@@ -1,7 +1,7 @@
-// Example SyntaxError
-//Missing a variable name
-//var = "food";
-var leg= "food";//The fixed version:
+console.log ("Example SyntaxError");
+console.log ("Missing a variable name");
+console.log ("var = "food"");
+console.log ("var leg= "food"");//The fixed version:
 // Reserved keywords can't be variable names
 //var char = "abc";
 // SyntaxError: missing variable name
@@ -14,7 +14,7 @@ var Charer= "abc"; //The fixed version:
 //var second = document.getElementById('two'),
  //SyntaxError: missing variable name
 
- var x, y = "foo";
+var x, y = "foo";
 var x = "foo";
 
 var first = document.getElementById('one');
