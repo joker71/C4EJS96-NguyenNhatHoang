b317157ea7eb1ac4e717190c969d6006e9362027
joker71 <nhathoangn0@gmail.com>
2020-09-16 20:35:00 +0700

    change

--- Session1/Bai4/Bai4.js
+++ Session1/Bai4/Bai4.js
@@ -1,6 +1,6 @@
 var messenger = "Coding might not be easy, but still great";
 console.log(messenger);
-messenger= messenger.toUpperCase();
+messenger= messenger.toLowerCase();
 console.log(messenger);
 var studentCount= 16;
 console.log(studentCount);
