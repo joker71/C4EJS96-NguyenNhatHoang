5e7b9712e2c3e0f809cf0fa2a3259efc5e54a148
joker71 <nhathoangn0@gmail.com>
2020-09-19 20:56:41 +0700

    change

--- Session3/video_1/video_1.js
+++ Session3/video_1/video_1.js
@@ -1,6 +1,29 @@
 
 let movie = ['Tenet', 'Inception', 'Dark knight', 'Doraemon', 'Naruto'];
 console.log(movie);
-let newItem = prompt('Enter new movie you like');
-movie.push(newItem);
-console.log(movie)
+function creatArray() {
+    let newItem = prompt('Enter new movie you like');
+    movie.push(newItem);
+    console.log(movie)
+}
+function readArray() {
+    let i = prompt('Enter item you want to see');
+    alert(movie[i]);
+}
+function deleteOne()
+{
+    let pos= prompt('Enter item posttion you want to delete');
+    movie.splice(pos, 1);
+    console.log(movie);
+}
+function deleteMore()
+{
+    let pos=  prompt('Enter item posttion you want to delete');
+    let n =  prompt('How many items do you want to delete');
+    movie.splice(pos, n);
+    console.log(movie);
+}
+//creatArray();
+//readArray();
+//deleteOne();
+deleteMore();
