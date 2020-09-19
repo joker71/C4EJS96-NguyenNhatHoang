cfb23e3d467dd25bf48fc3e9507a56702d9cb715
joker71 <nhathoangn0@gmail.com>
2020-09-19 19:40:40 +0700

    Command

--- Session2/Bai9b/Bai9b.js
+++ Session2/Bai9b/Bai9b.js
@@ -4,7 +4,7 @@ let countL = 0;
 let countH = 0;
 for (let i = 0; i < n; i++) {
    if (i < b) {
-      console.log("L");
+      console.log("L"); // thay vì in ra nhiều H sẽ in ra (khoanh tròn số) L
    }
    else if (i > b) {
       console.log("H");
