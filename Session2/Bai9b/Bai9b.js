3b964e66cc335e4db7ebd3cbcd5a56bdccb66208
joker71 <nhathoangn0@gmail.com>
2020-09-19 19:39:43 +0700

    change

--- Session2/Bai9b/Bai9b.js
+++ Session2/Bai9b/Bai9b.js
@@ -4,11 +4,9 @@ let countL = 0;
 let countH = 0;
 for (let i = 0; i < n; i++) {
    if (i < b) {
-      countL++;
+      console.log("L");
    }
    else if (i > b) {
-      countH++;
+      console.log("H");
    }
 }
-console.log(countH + " H");
-console.log(countL + " L");
