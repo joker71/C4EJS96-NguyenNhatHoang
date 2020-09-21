f9aaa0061d53fbdddf27b9a8e056ceb9493c86a4
joker71 <nhathoangn0@gmail.com>
2020-09-21 21:01:58 +0700

    Change

--- Session3/Hwork4/hwork.js
+++ Session3/Hwork4/hwork.js
@@ -1,18 +1,18 @@
 let order;
 let arr = ["T-Shirt", "Jeans", "Shocks"];
-for(let i=1; i<=2; i++) {
+for (let i = 1; i <= 2; i++) {
     order = prompt("Hi there, welcome to admin shop panel, what do you want to do(C, R, U, D)");
     if (order == "C" || order == "c") {
         let newItem = prompt("Enter new Item");
         arr.push(newItem);
-        console.log("THe curent list is:")
-        console.log(arr);
+        alert("THe curent list is:")
+        alert(arr);
         alert("Done!");
     }
     else
         if (order == "r" || order == "R") {
-            console.log("THe curent list is:")
-            console.log(arr);
+            alert("THe curent list is:")
+            alert(arr);
             alert("Done!");
         }
         else
@@ -20,20 +20,19 @@ for(let i=1; i<=2; i++) {
                 let pos = prompt("Enter position you want to update");
                 let Item = prompt("Enter the item");
                 arr[pos + 1] = Item;
-                console.log("THe curent list is:")
-                console.log(arr);
+                alert("THe curent list is:")
+                alert(arr);
                 alert("Done!");
             }
             else
                 if (order == "u" || order == "U") {
                     let pos = prompt("Enter position you want to delete");
                     arr.splice(pos);
-                    console.log("THe curent list is:")
-                    console.log(arr);
+                    alert("THe curent list is:")
+                    alert(arr);
                     alert("Done!");
                 }
-                else
-                {
+                else {
                     alert("this comand is not supported!");
                 }
 }
