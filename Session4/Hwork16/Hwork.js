b108291ff71edd2bf7505e55d9023b024d6f29ff
joker71 <nhathoangn0@gmail.com>
2020-09-26 20:06:05 +0700

    ssas

---
+++ Session4/Hwork16/Hwork.js
@@ -0,0 +1,24 @@
+const oldData = {
+  firedRice: {
+      Price: 30,
+      vnName: "Com rang dua bo"
+  },
+  noddle: {
+      price: 20,
+      vnName: "My tom chanh"
+  },
+  pho: {
+      price: 35,
+      vnName: "Pho bo tai chin"
+  },
+  };
+
+  let newData={}
+  for(let item in oldData)
+  {
+    if((item=="firedRice")|| (item="pho"))
+    {
+      newData[item]= oldData[item];
+    }
+  }
+  console.log(newData);
