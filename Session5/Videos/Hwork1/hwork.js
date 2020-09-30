27394e504a01080744dfba084553de953b2370f2
joker71 <nhathoangn0@gmail.com>
2020-09-30 19:51:02 +0700

    fixx

---
+++ Session5/Videos/Hwork1/hwork.js
@@ -0,0 +1,56 @@
+let Quiz=[
+    {
+        que: "How many day on the week",
+        choi: [1, 8, 9, 7],
+        ans: 7,
+    },
+    {
+        que: "How many legs do an optopus has",
+        choi: [1, 8, 9, 7],
+        ans: 8,
+    },
+    {
+        que: "How many  sesson in VietNam",
+        choi: [1, 8, 4, 7],
+        ans: 4,
+    },
+    {
+        que: "How many day on the week",
+        choi: [1, 8, 9, 7],
+        ans: 7,
+    },
+    {
+        que: "How many legs do an optopus has",
+        choi: [1, 8, 9, 7],
+        ans: 8,
+    },
+    {
+        que: "How many  sesson in VietNam",
+        choi: [1, 8, 4, 7],
+        ans: 4,
+    },
+];
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+let quizLength= Quiz.length;
+let mixQuiz= shuffle(Quiz);
+let totalRight= 0;
+for(let i=0; i< quizLength; i++)
+{
+    let mixQues= shuffle(mixQuiz[i].choi);
+    let choice= prompt(`${mixQuiz[i].que} \n ${mixQues}`);
+    if(choice==mixQuiz[i].choi)
+    {
+        totalRight++;
+    }
+}
+alert("We are out of question")
+alert(`Score: ${totalRight}`);
