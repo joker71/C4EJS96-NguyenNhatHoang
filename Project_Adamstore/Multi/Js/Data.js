ba9c347b2ead8ff05e44f2826e59eab0f5e50696
joker71 <nhathoangn0@gmail.com>
2020-10-11 22:04:09 +0700

    Add data

---
+++ Project_Adamstore/Multi/Js/Data.js
@@ -0,0 +1,39 @@
+let product= [
+    {
+        name: "Thriller",
+        singer: "Michael Jackson",
+        kind: "Dance & Electronic",
+        pic: "https://images-na.ssl-images-amazon.com/images/I/81wfye0hsDL._SL1500_.jpg",
+        des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis quae",
+        year: 1991,
+        list: ["Human nature", "The Lady in my life", "Baby be mine", "Thrill"],
+        price: 300000,
+        dim: '12.2 x 12.3 x 0.32 inches; 11.2 Ounces',
+
+    },
+    {
+        name: "Moby Play",
+        singer: "Moby",
+        kind: "Ambient",
+        pic: "https://m.media-amazon.com/images/I/51z8fXAuvjL._AC_UY327_FMwebp_QL65_.jpg",
+        des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis quae",
+        year: 1999,
+        list: ["Honey", "Find my baby", "Run on", "Body rock", "South side"],
+        price: 300000,
+        dim: '12.2 x 12.3 x 0.32 inches; 11.2 Ounces',
+
+    },
+    {
+        name: "Moby Play",
+        singer: "Moby",
+        kind: "Pop",
+        pic: "https://m.media-amazon.com/images/I/51z8fXAuvjL._AC_UY327_FMwebp_QL65_.jpg",
+        des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis quae",
+        year: 1999,
+        list: ["Honey", "Find my baby", "Run on", "Body rock", "South side"],
+        price: 300000,
+        dim: '12.2 x 12.3 x 0.32 inches; 11.2 Ounces',
+
+    },
+
+]
