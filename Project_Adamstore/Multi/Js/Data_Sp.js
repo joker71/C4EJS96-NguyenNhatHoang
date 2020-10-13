eacf1986f420b036e030714a0ba7f9b43dece01f
joker71 <nhathoangn0@gmail.com>
2020-10-13 11:53:52 +0700

    Add user data

--- Project_Adamstore/Multi/Js/Data_Sp.js
+++ Project_Adamstore/Multi/Js/Data_Sp.js
@@ -2,7 +2,7 @@ let product= [
     {
         name: "Thriller",
         singer: "Michael Jackson",
-        kind: "Dance & Electronic",
+        kind: "Old-1994",
         pic: "https://images-na.ssl-images-amazon.com/images/I/81wfye0hsDL._SL1500_.jpg",
         des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis quae",
         year: 1991,
@@ -14,26 +14,39 @@ let product= [
     {
         name: "Moby Play",
         singer: "Moby",
-        kind: "Ambient",
+        kind: "User-50%",
         pic: "https://m.media-amazon.com/images/I/51z8fXAuvjL._AC_UY327_FMwebp_QL65_.jpg",
         des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis quae",
         year: 1999,
         list: ["Honey", "Find my baby", "Run on", "Body rock", "South side"],
+        price: 360000,
+        dim: '12.2 x 12.3 x 0.32 inches; 11.2 Ounces',
+
+    },
+    {
+        name: "Frozen soutrack",
+        singer: "Disney",
+        kind: "New 100%",
+        pic: "https://images-na.ssl-images-amazon.com/images/I/81pWj%2BYv8KL._SL1500_.jpg",
+        des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis quae",
+        year: 2019,
+        list: ["All is found", "Into the unknown", "Lost in the woods", "The next right things", "When i am older", "Show yourshelf"],
         price: 300000,
         dim: '12.2 x 12.3 x 0.32 inches; 11.2 Ounces',
 
     },
     {
-        name: "Moby Play",
-        singer: "Moby",
-        kind: "Pop",
-        pic: "https://m.media-amazon.com/images/I/51z8fXAuvjL._AC_UY327_FMwebp_QL65_.jpg",
+        name: "Night at the Opera",
+        singer: "Disney",
+        kind: "Old-1965",
+        pic: "https://images-na.ssl-images-amazon.com/images/I/71zsO-uc34L._SY450_.jpg",
         des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis quae",
-        year: 1999,
-        list: ["Honey", "Find my baby", "Run on", "Body rock", "South side"],
-        price: 300000,
+        year: 1964,
+        list: ["All is found", "Into the unknown", "Lost in the woods", "The next right things", "When i am older", "Show yourshelf"],
+        price: 400000,
         dim: '12.2 x 12.3 x 0.32 inches; 11.2 Ounces',
 
     },
+    
 
 ]
