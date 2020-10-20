fa1675731fc68a91e50e706be1dd1b0f4f1e2f2f
joker71 <nhathoangn0@gmail.com>
2020-10-20 20:03:43 +0700

    Sort function

--- Project_Adamstore/Multi/Js/index.js
+++ Project_Adamstore/Multi/Js/index.js
@@ -1,7 +1,6 @@
-let mainPro= document.getElementById("showList");
-function soProduct(pic, name, des, price)
-{
-    mainPro.insertAdjacentHTML('beforeEnd', 
+let mainPro = document.getElementById("showList");
+function soProduct(pic, name, des, price) {
+  mainPro.insertAdjacentHTML('beforeEnd',
     `
     <div class="productItem">
       <div class="productPic"><img src="./Multi/img/${pic}" alt=""></div>
@@ -13,11 +12,52 @@ function soProduct(pic, name, des, price)
       <button class="addPro">Add to cart</button>
      </div>
     `
-    )
+  )
 }
-mainPro.innerHTML = '';
-for(let i=0; i< product.length; i++)
-{
+
+// mainPro.innerHTML = '';
+// for (let i = 0; i < product.length; i++) {
+//   soProduct(product[i].pic, product[i].name, product[i].des, product[i].price);
+// }
+
+// const showMore=document.getElementsByClassName("showMore");
+// showMore.addEventListener('change', ()=>{
+
+//   let sortProd = showMore.value;
+//  console.log(sortProd);
+// });
+function showProduct() {
+  mainPro.innerHTML = '';
+  for (let i = 0; i < product.length; i++) {
     soProduct(product[i].pic, product[i].name, product[i].des, product[i].price);
+  }
 }
-// mainPro.insertAdjacentHTML
+showProduct();
+const sortPro = document.getElementById("sortbyprice");
+sortPro.addEventListener('change', (e) => {
+  let sortType = sortPro.value;
+  console.log(sortType);
+  console.log(typeof (sortType));
+  if (sortType == "hight_to_low") {
+    product.sort(function (a, b) {
+      let keyA = a.price;
+      let keyB = b.price;
+      if (keyA < keyB) return -1;
+      if (keyA > keyB) return 1;
+      return 0;
+    });
+    showProduct();
+  }
+  else
+  {
+    product.sort(function (a, b) {
+      let keyA = a.price;
+      let keyB = b.price;
+      if (keyA < keyB) return 1;
+      if (keyA > keyB) return -1;
+      return 0;
+    });
+    showProduct();
+
+  }
+});
